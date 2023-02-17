9a4c03d0079492e842e4ff16205bedfc1bd32fc8
Hardik <hardikg2907@gmail.com>
2023-02-18 01:03:12 +0530

    connected to db and made login/signup frontend

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,31 @@
 //http://127.0.0.1:3000/login.html
 
 const express = require('express');
-const app = express();  
-const port = 3000;
+const app = express();
+const mongoose = require('mongoose');
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const User = require('./User.js')
 
 app.use(express.static('../frontend'))
 
 app.get('/', (req,res)=>{
-    // res.send('Hello')
+    res.send();
+    // console.log(res);
+})
+
+app.post('/register',async (req,res)=>{
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+    });
+
+    return res.status(200).json(user);
 })
 
 app.listen(port, ()=>{
+    mongoose.connect(process.env.DB_URI, ()=>{
+        console.log('DB Connected Successfully');
+    });
     console.log(`Server is listening on port ${port}...`);
 })
--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,9 @@
-alert("hello");
+// alert("hello");
+let login=document.getElementById("login");
+let signup=document.getElementById("signup");
+
+signup.addEventListener('click',function handleclick()
+{
+    document.getElementById("sigupbox").style.display="block";
+    document.getElementById("login-box").style.display="none";
+})
