9cdc2c2910c627e617d3c59abf0a1430b14ae3a9
Hardik <hardikg2907@gmail.com>
2023-02-18 14:41:28 +0530

    register and login working

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,41 @@ const User = require('./User.js')
 
 app.use(express.static('../frontend'))
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/login', async (req,res)=>{
+app.post('/login', async (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
 
-    
+    await User.findOne({ username: `${username}` }
+        // if(err)
+        // {
+        //     console.log('Not found user');
+        //     return;
+        // }
+        // if(password === user.password)
+        // {
+        //     console.log('User found');
+        //     return;
+        // }
+    )
+        .then(user => {
+            if (password === user.password) {
+                console.log('User found')
+                return
+            }
+            // else {
+            //     console.log('User not found.')
+            //     return
+            // }
+        })
+        .catch(err=>{
+            console.log('error');
+            return
+        })
 })
 
-app.post('/register',async (req,res)=>{
+app.post('/register', async (req, res) => {
     console.log(req.body);
 
     const user = new User({
@@ -32,8 +57,8 @@ app.post('/register',async (req,res)=>{
     return res.status(200).json(user);
 })
 
-app.listen(port, ()=>{
-    mongoose.connect(process.env.DB_URI, ()=>{
+app.listen(port, () => {
+    mongoose.connect(process.env.DB_URI, () => {
         console.log('DB Connected Successfully');
     });
     console.log(`Server is listening on port ${port}...`);
